76e7d3d15a0030c44d7356535a40fb7db9d87cae
てりほー <87942773+p-yttor4869@users.noreply.github.com>
2022-04-22 00:50:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,15 +5,8 @@ import platform
 import json
 from colorama import Fore
 from modules import updater
-try:
-  import nextcord
-  from nextcord.ext import commands
-except ModuleNotFoundError:
-  print(Fore.RED + "ライブラリの読み込みに失敗しました\nインストールを実行します" + Fore.RESET)
-  os.system("pip install -r bot/requirements.txt") 
-  os.system("clear")
-  print(Fore.GREEN + "ライブラリのインストールが完了しました\nもう一度起動してください" + Fore.RESET)
-  sys.exit(1)  
+import nextcord
+from nextcord.ext import commands
 
 updater.updater()
 with open('config.json','r') as f:
