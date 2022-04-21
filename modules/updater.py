91abee5218dc6e990ad0445e09da5c2c9678c6e1
てりほー <87942773+p-yttor4869@users.noreply.github.com>
2022-04-21 17:23:15 +0900

    Update updater.py

--- modules/updater.py
+++ modules/updater.py
@@ -8,20 +8,20 @@ def updater():
   with open('config.json','r') as f:
     config = json.load(f)
   with open('bot.json','r') as f:
-    bot = json.load(f)
+    bots = json.load(f)
 
-  version = bot['version']  
-  git = f"{bot['git_raw']}/{bot['git_branch']}"
+  version = bots['version']  
+  git = f"{bots['git_raw']}/{bots['git_branch']}"
   new_version = requests.get(f"{git}/bot.json").json()['version']
   if version != new_version:
     print(Fore.GREEN + f"バージョン{new_version}へのアップデートを確認しました\nアップデートを実行します" + Fore.RESET)
-    files = os.listdir("modules")
-    for file in files:
-      if file.startswith("_"):
-        continue
-      new_file = requests.get(f"{git}/modules/{file}").text
-      with open(f'modules/{file}','w') as f:
-        f.write(new_file)
+    for path in bots['module_paths']:    
+      for file in os.listdir(path):
+        if file.startswith("_"):
+          continue
+        new_file = requests.get(f"{git}/modules/{file}").text
+        with open(f'modules/{file}','w') as f:
+          f.write(new_file)
     new_config = requests.get(f"{git}/config.json").json()
     for key in config:
       db[key] = str(config[key])
@@ -39,4 +39,7 @@ def updater():
       bot['version'] = new_version
     with open('bot.json','w') as f:
       json.dump(bot,f,indent=2)    
-    print(Fore.GREEN + f"バージョン{new_version}へのアップデートが正常に完了しました" + Fore.RESET)    
+    print(Fore.GREEN + f"バージョン{new_version}へのアップデートが正常に完了しました" + Fore.RESET)
+  else:
+    print(Fore.GREEN + "アップデートは見つかりませんでした" + Fore.RESET)   
+
