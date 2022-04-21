be0127e8957b9a880116452937ca8ccdf12c624d
てりほー <87942773+p-yttor4869@users.noreply.github.com>
2022-04-22 00:33:46 +0900

    Update updater.py

--- modules/updater.py
+++ modules/updater.py
@@ -17,8 +17,8 @@ def updater():
   if version != new_version:
     print(Fore.GREEN + f"バージョン{new_version}へのアップデートを確認しました\nアップデートを実行します" + Fore.RESET)
     new_files = requests.get(f"{git}/files.json").json()
-    with open('files.json','w') as f:
-      json.dump(new_files,f,indent=2)
+    with open('files.json','w',encoding='utf-8') as f:
+      json.dump(new_files,f,indent=2,ensure_ascii=False)
     with open('files.json','r') as f:
       files = json.load(f)['files']   
     for file in files:
@@ -31,20 +31,20 @@ def updater():
     new_config = requests.get(f"{git}/config.json").json()
     for key in config:
       db[key] = str(config[key])
-    with open('config.json','w') as f:
-      json.dump(new_config,f,indent=2)
+    with open('config.json','w',encoding='utf-8') as f:
+      json.dump(new_config,f,indent=2,ensure_ascii=False)
     for key in db.keys():
       with open('config.json','r') as f:
         config = json.load(f)
         config[key] = str(db[key])
         del db[key]
-      with open('config.json','w') as f:
-        json.dump(config,f,indent=2)
+      with open('config.json','w',encoding='utf-8') as f:
+        json.dump(config,f,indent=2,ensure_ascii=False)
     with open('bots.json','r') as f:     
       bots = json.load(f)
       bots['version'] = new_version
-    with open('bots.json','w') as f:
-      json.dump(bots,f,indent=2)    
+    with open('bots.json','w',encoding='utf-8') as f:
+      json.dump(bots,f,indent=2,ensure_ascii=False)    
     print(Fore.GREEN + f"バージョン{new_version}へのアップデートが正常に完了しました" + Fore.RESET)
   else:
     print(Fore.GREEN + "アップデートは見つかりませんでした" + Fore.RESET)
